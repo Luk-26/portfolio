97b007fa044b5ccc4a418f2fd173d45d5d456ee8
luk26 <lucas.guillemaud@gmail.com>
2026-01-21 12:00:51 +0100

    Mise en place des premieres routes, supr de la page "par defaut"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Accueil } from './pages/accueil/accueil';
+import { Contact } from './pages/contact/contact';
+import { Projets } from './pages/projets/projets';
+import { Experiences } from './pages/experiences/experiences';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: Accueil, title: "Accueil - Portfolio Lucas GUILLEMAUD" },
+    { path: "projets", component: Projets, title: "Mes Projets - Portfolio Lucas GUILLEMAUD" },
+    { path: "experiences", component: Experiences, title: "Mes Expériences - Portfolio Lucas GUILLEMAUD" },
+    { path: "contact", component: Contact, title: "Contact - Portfolio Lucas GUILLEMAUD" },
+];
