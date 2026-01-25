a31c81865a087633ec2978fcb918f47abf7eab10
luk26 <lucas.guillemaud@gmail.com>
2026-01-25 23:56:00 +0100

    Portfolio V1

--- src/app/elements/footer/footer.ts
+++ src/app/elements/footer/footer.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { PERSONAL_INFO } from '../../core/app-constants';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
-
+  personalInfo = PERSONAL_INFO;
+  currentYear = new Date().getFullYear();
 }
